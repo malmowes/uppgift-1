ad2e896c5106d3e69e4225da9f573609e2b4101f
wes <malmowes@hotmail.com>
2024-09-17 15:31:13 +0200

    styles basetext

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,34 +8,40 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
 
-<View style={{ backgroundColor: 'skyblue', flex: 1,                }}>                           
-<View style={{ backgroundColor: 'red', height:100, 
-  justifyContent: "center", alignItems: "center" }}><Text>RÖD</Text></View>
+    <View style={{ backgroundColor: "skyblue", flex: 1, }}>
+      <View style={{
+        backgroundColor: "red", height: 100,
+        justifyContent: "center", alignItems: "center"
+      }}><Text style= {styles.baseText}>RÖD</Text></View>
 
 
-<View style={{ backgroundColor: 'white', height: 100, flexDirection: 'row' }}>
+      <View style={{ backgroundColor: "white", height: 100, flexDirection: "row" }}>
 
-<View style={{ backgroundColor: 'green', flex: 1 }}><Text>GRÖN</Text></View>
-<View style={{ backgroundColor: 'yellow', flex: 1  }}></View>
-</View>
+        <View style={{ backgroundColor: "green", flex: 1 }}>
+          <Text style= {styles.baseText}>GRÖN</Text></View>
+        <View style={{ backgroundColor: "yellow", flex: 1 }}></View>
+      </View>
 
-<View style={{ backgroundColor: 'white', height: 50, flexDirection: 'row' }}>
+      <View style={{ backgroundColor: "white", flexDirection: "row" }}>
 
-<View style={{ backgroundColor: 'red', flex: 3 }}></View>
-<View style={{ backgroundColor: 'black', flex: 1  }}></View>
-</View>
-
-
-
-
-
+        <View style={{ backgroundColor: "red", height: 100, flex: 3 }}></View>
+        <View style={{ backgroundColor: "black", height: 100, flex: 1 }}></View>
+      </View>
 
 
 
 
 
 
-</View>
+      <View style={{ flex: 1 }}></View>
+
+
+      <View style={{
+        backgroundColor: "orange", height: 50, justifyContent: "center",
+        alignItems: 'flex-end'
+      }}><Text style= {styles.baseText}>ORANGE</Text></View>
+
+    </View>
 
   );
 }
@@ -46,6 +52,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
+  baseText: {
+    fontWeight: "bold", fontSize: "25"
+  },
+
+
+
+
   stepContainer: {
     gap: 8,
     marginBottom: 8,
